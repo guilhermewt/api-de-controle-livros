79bd8fad185c0e483430adfa5de71292a72f100f
welliston <welliston10@gmail.com>
2022-05-29 16:29:24 -0300

    metodos de CRUD do usuario

--- src/main/java/com/biblioteca/services/serviceUsuario.java
+++ src/main/java/com/biblioteca/services/serviceUsuario.java
@@ -17,4 +17,30 @@ public class serviceUsuario {
 	public List<Usuario> findAll(){
 		return service.findAll();
 	}
+	
+	public Usuario findById(long id) {
+		return service.findById(id).get();
+	}
+	
+	public Usuario insert(Usuario obj) {
+		return service.save(obj);
+	}
+	
+	public void delete(long id) {
+		findById(id);
+		service.deleteById(id);
+	}
+	
+	public Usuario update(Usuario obj) {
+		Usuario usuario = service.findById(obj.getId()).get();
+		updateData(usuario,obj);
+		return service.save(usuario);
+	}
+
+	private void updateData(Usuario usuario, Usuario obj) {
+		usuario.setNome(obj.getNome());
+		usuario.setEmail(obj.getEmail());
+		usuario.setLogin(obj.getLogin());
+		usuario.setSenha(obj.getSenha());
+	}
 }
